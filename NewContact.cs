9abf7b88c2ec06cabe0ac3ccac95db64fa89eed3
murgendrag <murgendra67@gmail.com>
2022-03-10 17:16:34 +0530

    contact created

---
+++ NewContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        public void AddContact()
+        {
+            string Name;
+            Console.WriteLine("Enter FIRST name and LAST name");
+            Name = Console.ReadLine();
+            string Address;
+            Console.WriteLine("Enter Address city state zip");
+            Address = Console.ReadLine();
+            string EmailId;
+            Console.WriteLine("Enter EmailId");
+            EmailId = Console.ReadLine();
+            long Phonenumber;
+            Console.WriteLine("Enter your PhoneNumber");
+            Phonenumber = Convert.ToInt64(Console.ReadLine());
+            
+        }
+    }
+}
