c8fd8a3d53e919955b088abd46a4587e9e3ab855
murgendrag <murgendra67@gmail.com>
2022-03-11 18:32:51 +0530

    new contact

---
+++ Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_Day9
+{
+    public class Contact
+    {
+        public string FirstName { get; set; } //string property for first ,second name , phone number
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+        public string[] Address { get; set; } //string array to hold two addresses
+    }
+}
